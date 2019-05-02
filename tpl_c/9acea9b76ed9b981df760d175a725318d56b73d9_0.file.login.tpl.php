b5db51828669ede1f563aa17f8924ef68ff83520
root <david.higuera.ferrez@gmail.com>
2019-05-02 17:04:06 +0200

    Initial commit

--- tpl_c/9acea9b76ed9b981df760d175a725318d56b73d9_0.file.login.tpl.php
+++ tpl_c/9acea9b76ed9b981df760d175a725318d56b73d9_0.file.login.tpl.php
@@ -1,11 +1,11 @@
 <?php
-/* Smarty version 3.1.30, created on 2019-05-02 16:59:44
+/* Smarty version 3.1.30, created on 2019-05-02 17:02:13
   from "/var/www/reservaespacios/html/booked/tpl/login.tpl" */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.30',
-  'unifunc' => 'content_5ccb05e0eafa10_05488415',
+  'unifunc' => 'content_5ccb0675513290_52985301',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
@@ -23,7 +23,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
     'file:globalfooter.tpl' => 1,
   ),
 ),false)) {
-function content_5ccb05e0eafa10_05488415 (Smarty_Internal_Template $_smarty_tpl) {
+function content_5ccb0675513290_52985301 (Smarty_Internal_Template $_smarty_tpl) {
 ?>
 
 <?php $_smarty_tpl->_subTemplateRender("file:globalheader.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
